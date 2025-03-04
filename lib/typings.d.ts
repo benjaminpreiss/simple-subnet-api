6d80597e5f201f9cc176332963af4f04f3f797c2
Srdjan <stankovic.srdjo@gmail.com>
2025-03-04 14:20:46 +0100

    Update lib/typings.d.ts
    Co-authored-by: Miroslav Bajtoš <oss@bajtos.net>

--- lib/typings.d.ts
+++ lib/typings.d.ts
@@ -8,7 +8,6 @@ export interface Logger {
 
 export type PgPool = pg.Pool;
 
-// Copied from import('@types/pg').
 export type Queryable = Pick<Pool, "query">;
 export type UnknownRow = Record<string, unknown>;
 export type QueryResultWithUnknownRows = pg.QueryResult<UnknownRow>;
